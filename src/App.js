e34e9cd3595aeb1ddb6123331c612f95173ec5ec
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-09-05 11:40:52 +0530

    refactor: move header UI from App.js into Hero.js component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
+import Hero from './Hero';
 
 function App() {
   return (
-    <section className="min-h-screen flex items-center justify-center">
-      <div className="text-left">
-        <h1 className="text-3xl font-light text-gray-400">
-          This is <span className="font-bold text-orange-500">IDEA YARD</span>
-          <br />
-          by <span className="font-bold text-black">Sukhdev Thukral.</span>
-        </h1>
-      </div>
-    </section>
+    <>
+      <Hero />
+    </>
   );
 }
 
---
+++ src/Hero.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <div className="text-left">
+        <h1 className="text-3xl font-light text-gray-400">
+          This is <span className="font-bold text-orange-500">IDEA YARD</span>
+          <br />
+          by <span className="font-bold text-black">Sukhdev Thukral.</span>
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default App;
